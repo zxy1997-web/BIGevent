4edbb3722dd1232ceae9cfefd92ae806fc0ab08f
zxy1997-web <627345391@qq.com>
2019-11-23 14:32:01 +0800

    增加项目

--- web_back/js/utile/category.js
+++ web_back/js/utile/category.js
@@ -3,5 +3,15 @@ var category = {
         $.get(APIURLS.category_show,function(res){
             callback(res)
         })
+    },
+    del: function(id,callback){
+        $.post(APIURLS.category_del,{'id' : id},function(res){
+            callback(res)
+        })
+    },
+    add:function(name,slug,callback){
+        $.post(APIURLS.category_add,{'name':name,'slug':slug},function(res){
+            callback(res)
+        })
     }
-}
+} 
