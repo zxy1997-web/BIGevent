feb86e20ee2564c0f00248da875aa3093ccdfc9b
zxy1997-web <627345391@qq.com>
2019-11-24 10:53:42 +0800

    编辑功能

--- web_back/js/utile/category.js
+++ web_back/js/utile/category.js
@@ -13,5 +13,10 @@ var category = {
         $.post(APIURLS.category_add,{'name':name,'slug':slug},function(res){
             callback(res)
         })
-    }
+    },
+    edit:function(id,name,slug,callback){
+        $.post(APIURLS.category_edit,{'id':id,'name':name,'slug':slug},function(res){
+            callback(res)
+        })
+    },
 } 
